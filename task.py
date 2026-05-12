96b7a043b164ae5c799db623bc2662d506095bc0
Aliaksei <fanikov41@gmail.com>
2026-05-12 21:12:56 +0300

    task 7.08

--- task.py
+++ task.py
@@ -141,8 +141,30 @@ def test_2(*args, **kwrags):
 
 def search_key(**kwargs):
     for key, val in kwargs.items():
-        if len(key)%2==0:
+        if len(key) % 2 == 0:
             print(f'key - {key} val - {val}')
-            
-            
-search_key(asd=4, aw=2, asdfg=5)
+
+
+def arithmetic_mean(*args):
+    return round(sum(args) / len(args),3)
+
+
+def geometric_mean(*args):
+    product = 1
+    for x in args:
+        product *= x
+    return round(product ** (1 / len(args)), 3)
+
+
+def mean_with_type(*args, mean_type="arithmetic"):
+    t = str(mean_type).lower().strip()
+    if t == 'arithmetic':
+        return arithmetic_mean(*args)
+    if t == 'geometric':
+        return geometric_mean(*args)
+    else:
+        print('ERROR')
+
+
+print(mean_with_type(2, 4, 8, mean_type="arithmetic"))
+print(mean_with_type(2, 4, 8, mean_type="geometric"))
